60d8eb752fe86f906bee00af6b760fe5e58214c5
RaviKirankella <ravikiran59@gmail.com>
2017-05-24 15:52:20 +0100

    Update PBCalc.js

--- PBCalc.js
+++ PBCalc.js
@@ -19,42 +19,25 @@ function getval(val) {
 }
 
 function opplist() {
-	var opp =  [
-"Aldworth",
-"Appleton",
-"Astons",
-"Bartlemas C.C. (formerly Probationers)",
-"Blackwells",
-"Blenheim Park",
-"Bodleian Library",
-"Charlton-on-Otmoor",
-"Cherwell School Staff",
-"Cholsey",
-"Enstone",
-"Far from the MCC",
-"Garsington",
-"HCL",
-"Ibstone",
-"Iffle-y Village",
-"Infosys",
-"Ipsden",
-"Islip",
-"Keen Crcketers",
-"Kempsford",
-"Kingston Bagpuize",
-"Lord Williams Staff",
-"Neilsen Media Research",
-"Nomads C.C.",
-"Oxford University Press",
-"Oxfordshire County Council",
-"Quokkers",
-"Railway Taverners",
-"United Oxford Hospitals",
-"Wolvercote",
-"Wooton & Boars Hill",
-"Wytham",
-"Other Team"
-    ];
+	var opp =  ["Aldworth", "Appleton", "Astons", "Bartlemas C.C. (formerly Probationers)", "Blackwells", "Blenheim Park",
+		    "Bodleian Library", "Charlton-on-Otmoor", "Cherwell School Staff", "Cholsey", "Enstone", "Far from the MCC", 
+		    "Garsington", "HCL", "Ibstone", "Iffle-y Village", "Infosys", "Ipsden","Islip","Keen Crcketers","Kempsford",
+		    "Kingston Bagpuize","Lord Williams Staff","Neilsen Media Research","Nomads C.C.","Oxford University Press",
+		    "Oxfordshire County Council","Quokkers","Railway Taverners","United Oxford Hospitals","Wolvercote",
+		    "Wooton & Boars Hill","Wytham","Other Team"];
 	return opp;
 }
+
+function playerlist() {
+	var players =  ["A. Albasri", "A. East", "A. Mohammad", "A. Shariff", "A. Symons", "B. Harden", "B. Shepherd", "C. Tarratt", 
+			"D. Henry", "D. Penhallurick", "D. Valsan", "D.J. Lawrence", "G. Barker", "G. Gardner-Morrison", "H. Hudson", 
+			"H. Stoneman", "J. Gifford", "J. Khawaja", "J. Ponsford", "J. Walter", "K. Bhaskaran", "K. Ponsford", 
+			"K. Siddireddy", "K.H. Whiter", "L. Blatchford", "L. Whitby", "M. Naqvi", "M. Stanbury", "M. Thompson", 
+			"N. Hardy", "N. Hussain", "N. Khawaja", "N.H.R. Wyatt", "O. Walter", "P. Davenport", "P. Gregory", 
+			"P. Jacobs", "P. Wiblin", "R. Blatchford", "R. Kella", "R. Stephenson", "R. Stoneman", "R. Tolson", 
+			"R. Vellayappan", "R. Venkat", "R. Yennapusa", "S. Gregan", "S. Khawaja", "S. Mistry", "S. Sivalingam", 
+			"W. Green", "Other Player"];
+	return players;
+}
+
 
