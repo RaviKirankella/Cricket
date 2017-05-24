0d53e2c396ce6756e3e23629bfff1e243aa20bc2
RaviKirankella <ravikiran59@gmail.com>
2017-05-24 14:37:05 +0100

    Update PBCalc.js

--- PBCalc.js
+++ PBCalc.js
@@ -1,11 +1,12 @@
-function calc() {
+function calc1() {
 	document.getElementById("test").innerHTML="Good";
 }
 
-function calc1() {
+function calc() {
 	var x = document.getElementsByClassName("field");
 	var i;
 	for (i = 0; i < x.length; i++) {
 		val = val+x[i].nodeValue;
 	}
+	document.getElementById("test").innerHTML=val;
 }
