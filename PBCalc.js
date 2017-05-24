42d694ef63278d5099b5c8ac666200ad725e9c71
RaviKirankella <ravikiran59@gmail.com>
2017-05-24 17:12:11 +0100

    Update PBCalc.js

--- PBCalc.js
+++ PBCalc.js
@@ -6,38 +6,29 @@ function calc() {
 	var x = document.getElementsByClassName("field");
 	var i;
 	var val="";
+	var adv=0;
+	
 	for (i = 0; i < x.length; i++) {
-		val = val+x[i].value;
+		val = x[i].value;
+		if (val =="") {
+			adv +=-2.5;
+		}
+		else {
+			adv += parseFloat(getval(val));
+		}
+		
 	}
-	document.getElementById("test").innerHTML=val;
+	adv = Math.exp(-adv);
+	adv = 100/(1+adv);
+	adv = Math.round(adv);
+	document.getElementById("test").innerHTML=adv;
 }
 
 function getval(val) {
-	if (val=="") {return 1;}
-	else if (val==" ") {return 0;}
-	else {return 0;}
-}
-
-function opplist() {
-	var opp =  ["Aldworth", "Appleton", "Astons", "Bartlemas C.C. (formerly Probationers)", "Blackwells", "Blenheim Park",
-		    "Bodleian Library", "Charlton-on-Otmoor", "Cherwell School Staff", "Cholsey", "Enstone", "Far from the MCC", 
-		    "Garsington", "HCL", "Ibstone", "Iffle-y Village", "Infosys", "Ipsden","Islip","Keen Crcketers","Kempsford",
-		    "Kingston Bagpuize","Lord Williams Staff","Neilsen Media Research","Nomads C.C.","Oxford University Press",
-		    "Oxfordshire County Council","Quokkers","Railway Taverners","United Oxford Hospitals","Wolvercote",
-		    "Wooton & Boars Hill","Wytham","Other Team"];
-	return opp;
-}
-
-function playerlist() {
-	var players =  ["A. Albasri", "A. East", "A. Mohammad", "A. Shariff", "A. Symons", "B. Harden", "B. Shepherd", "C. Tarratt", 
-			"D. Henry", "D. Penhallurick", "D. Valsan", "D.J. Lawrence", "G. Barker", "G. Gardner-Morrison", "H. Hudson", 
-			"H. Stoneman", "J. Gifford", "J. Khawaja", "J. Ponsford", "J. Walter", "K. Bhaskaran", "K. Ponsford", 
-			"K. Siddireddy", "K.H. Whiter", "L. Blatchford", "L. Whitby", "M. Naqvi", "M. Stanbury", "M. Thompson", 
-			"N. Hardy", "N. Hussain", "N. Khawaja", "N.H.R. Wyatt", "O. Walter", "P. Davenport", "P. Gregory", 
-			"P. Jacobs", "P. Wiblin", "R. Blatchford", "R. Kella", "R. Stephenson", "R. Stoneman", "R. Tolson", 
-			"R. Vellayappan", "R. Venkat", "R. Yennapusa", "S. Gregan", "S. Khawaja", "S. Mistry", "S. Sivalingam", 
-			"W. Green", "Other Player"];
-	return players;
+	var list = getlist("list");
+	var values = getlist("values");
+	var index = list.indexOf("val")
+	return values[index];
 }
 
 function getlist(type) {
@@ -74,7 +65,8 @@ function getlist(type) {
 	
 	if (type=="Players") {return list.slice(playerstartindex, playerendindex+1);}
 	else if (type=="Opposition") {return list.slice(oppstartindex);}
-	else if (type=="All") {return list;}
+	else if (type=="list") {return list;}
+	else if (type=="values") {return values;}
 	else {return list;}
 }
 
