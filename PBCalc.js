dbc2abd85070476b10b8bc4cc4bff3c33e63e224
RaviKirankella <ravikiran59@gmail.com>
2017-05-24 15:47:33 +0100

    Update PBCalc.js

--- PBCalc.js
+++ PBCalc.js
@@ -11,3 +11,50 @@ function calc() {
 	}
 	document.getElementById("test").innerHTML=val;
 }
+
+function getval(val) {
+	if (val=="") {return 1;}
+	else if (val==" ") {return 0;}
+	else {return 0;}
+}
+
+function opplist() {
+	var opp =  [
+"Aldworth",
+"Appleton",
+"Astons",
+"Bartlemas C.C. (formerly Probationers)",
+"Blackwells",
+"Blenheim Park",
+"Bodleian Library",
+"Charlton-on-Otmoor",
+"Cherwell School Staff",
+"Cholsey",
+"Enstone",
+"Far from the MCC",
+"Garsington",
+"HCL",
+"Ibstone",
+"Iffle-y Village",
+"Infosys",
+"Ipsden",
+"Islip",
+"Keen Crcketers",
+"Kempsford",
+"Kingston Bagpuize",
+"Lord Williams Staff",
+"Neilsen Media Research",
+"Nomads C.C.",
+"Oxford University Press",
+"Oxfordshire County Council",
+"Quokkers",
+"Railway Taverners",
+"United Oxford Hospitals",
+"Wolvercote",
+"Wooton & Boars Hill",
+"Wytham",
+"Other Team"
+    ];
+	return opp;
+}
+
