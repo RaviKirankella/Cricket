b0b9c97b1f69faf93b7bd71206be655685e4455a
RaviKirankella <ravikiran59@gmail.com>
2019-11-29 15:35:22 +0000

    Update FantasyLeague.js

--- FantasyLeague.js
+++ FantasyLeague.js
@@ -2,3 +2,13 @@ function getlist(type) {
 	var list = ["O. Walter"];	
 return list;
 }
+
+function getdata(player) {
+var playerdata = {"O. Walter":[ { y: 500, label: "2015" },
+				{ y: 600,  label: "2016" },
+				{ y: 505,  label: "2017" },
+				{ y: 800,  label: "2018" },
+				{ y: 205,  label: "2019" }]
+		 };	
+return playerdata["O. Walter"];
+}
